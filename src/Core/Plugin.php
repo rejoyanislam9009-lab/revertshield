51843e64f44adca75540766fced081039cc11ca5
rejoyanislam9009-lab <rejoyanislam9009@gmail.com>
2026-08-10 21:41:34 +0300

    feat: register snapshot admin and cleanup services

--- src/Core/Plugin.php
+++ src/Core/Plugin.php
@@ -9,10 +9,14 @@ namespace RevertShield\Core;
 
 use RevertShield\Admin\AdminPage;
 use RevertShield\Admin\Settings;
+use RevertShield\Admin\SnapshotAdminPage;
 use RevertShield\Database\Migrator;
 use RevertShield\Health\HealthChecker;
 use RevertShield\Ledger\ChangeObserver;
 use RevertShield\Ledger\ChangeRepository;
+use RevertShield\Snapshot\PluginSnapshotService;
+use RevertShield\Snapshot\SnapshotCleanup;
+use RevertShield\Snapshot\SnapshotRepository;
 use RevertShield\Support\Cleanup;
 
 /**
@@ -27,12 +31,15 @@ final class Plugin {
 	public function boot() {
 		Migrator::maybe_upgrade();
 
-		$repository = new ChangeRepository();
-		$health     = new HealthChecker();
+		$repository          = new ChangeRepository();
+		$health              = new HealthChecker();
+		$snapshot_repository = new SnapshotRepository();
 
 		( new ChangeObserver( $repository ) )->register();
 		( new Settings() )->register();
 		( new AdminPage( $repository, $health ) )->register();
+		( new SnapshotAdminPage( $snapshot_repository, new PluginSnapshotService(), $repository ) )->register();
 		( new Cleanup() )->register();
+		( new SnapshotCleanup( $snapshot_repository ) )->register();
 	}
 }
