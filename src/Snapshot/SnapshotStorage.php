c5b0ba1468e6fd500bf72d0c8160c345adc06523
rejoyanislam9009-lab <rejoyanislam9009@gmail.com>
2026-08-10 21:33:00 +0300

    style: align snapshot guard map

--- src/Snapshot/SnapshotStorage.php
+++ src/Snapshot/SnapshotStorage.php
@@ -268,7 +268,7 @@ final class SnapshotStorage {
 		$guards = array(
 			'.htaccess'  => "<IfModule mod_authz_core.c>\nRequire all denied\n</IfModule>\n<IfModule !mod_authz_core.c>\nDeny from all\n</IfModule>\n",
 			'web.config' => "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<configuration><system.webServer><authorization><deny users=\"*\" /></authorization></system.webServer></configuration>\n",
-			'index.php'   => "<?php\n// Silence is golden.\n",
+			'index.php'  => "<?php\n// Silence is golden.\n",
 		);
 
 		foreach ( $guards as $filename => $contents ) {
