ce41dbdf0ac8efb9e45a6339d3871de2580d263f
rejoyanislam9009-lab <rejoyanislam9009@gmail.com>
2026-08-10 21:01:01 +0300

    style: document uninstall schema cleanup

--- uninstall.php
+++ uninstall.php
@@ -20,7 +20,9 @@ global $wpdb;
 $changes     = $wpdb->prefix . 'revertshield_changes';
 $health_runs = $wpdb->prefix . 'revertshield_health_runs';
 
+// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.SchemaChange -- User explicitly opted into removing RevertShield's custom tables on uninstall.
 $wpdb->query( $wpdb->prepare( 'DROP TABLE IF EXISTS %i', $changes ) );
+// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.SchemaChange -- User explicitly opted into removing RevertShield's custom tables on uninstall.
 $wpdb->query( $wpdb->prepare( 'DROP TABLE IF EXISTS %i', $health_runs ) );
 
 delete_option( 'revertshield_settings' );
